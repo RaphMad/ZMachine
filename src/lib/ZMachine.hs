7e8b079a977a8aa9bab8e4f8da777961982f18eb
Raphael <RaphMad@users.noreply.github.com>
2016-02-16 16:25:06 +0100

    Implemented episode 'Up a tree'

--- src/lib/ZMachine.hs
+++ src/lib/ZMachine.hs
@@ -3,6 +3,7 @@ module ZMachine where
 import Data.Bits
 import ZMachine.ZTypes
 
+-- todo: move to ZTypes
 fetchBits :: BitNumber -> BitSize -> Number -> Number
 fetchBits (BitNumber high) (BitSize length) word =
   let mask = complement $ -1 `shiftL` length
--- src/lib/ZMachine/ZTypes.hs
+++ src/lib/ZMachine/ZTypes.hs
@@ -2,6 +2,8 @@
 
 module ZMachine.ZTypes where
 
+import Control.Applicative
+import qualified Data.Map as M
 import Data.Word
 
 type Number = Word16
@@ -32,3 +34,37 @@ size4 = BitSize 4
 size5 = BitSize 5
 size6 = BitSize 6
 size7 = BitSize 7
+
+data ByteAddress = ByteAddress Int
+
+isInRange :: ByteAddress -> Int -> Bool
+isInRange (ByteAddress address) size = 0 <= address && address < size
+
+isOutOfRange :: ByteAddress -> Int -> Bool
+isOutOfRange address size = not $ isInRange address size
+
+type IntMap = M.Map Int
+
+data ImmutableBytes = ImmutableBytes { originalBytes :: [Word8], edits :: IntMap Word8 }
+
+make :: [Word8] -> ImmutableBytes
+make bytes = ImmutableBytes bytes M.empty
+
+size :: ImmutableBytes -> Int
+size = length . originalBytes
+
+readByte :: ImmutableBytes -> ByteAddress -> Maybe Word8
+readByte bytes address@(ByteAddress addr) =
+   if isOutOfRange address (size bytes)
+   then Nothing
+   else M.lookup addr (edits bytes) <|>
+        Just (originalBytes bytes !! addr)
+
+writeByte :: ImmutableBytes -> ByteAddress -> Word8 -> Maybe ImmutableBytes
+writeByte bytes address@(ByteAddress addr) value =
+   if isOutOfRange address (size bytes)
+   then Nothing
+   else Just $ ImmutableBytes (originalBytes bytes) (M.insert addr value (edits bytes))
+
+newEdits :: ImmutableBytes -> IntMap Word8 -> ImmutableBytes
+newEdits (ImmutableBytes b _) = ImmutableBytes b
